1692fa6b77f1c8a83470ad4610c0321028d1a2ed
nadeemsangrasi <nadeemsangrasi903@gmail.com>
2024-03-21 10:39:03 +0500

    second commit

---
+++ Project-01 number guessing game/index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer"
+
+console.log("Welcome to my number guessing Game")
+console.log("You have 10 chances to win")
+let count:number=0
+while(count<11){
+    count++
+    let randomNumber : number = Math.floor(Math.random()*10)
+    let guessNumber =  await inquirer.prompt([{name:"guess",type:"number",message:"Guess number from 0 to 10"}])
+    if(guessNumber.guess>randomNumber){
+        console.log("Your guess is too high")
+        
+    }
+    else if(guessNumber.guess<randomNumber){
+        
+        console.log("Your guess is too low")
+    }
+    else{
+        console.log("Congratulations your guess is correc")
+        break
+    }
+
+}
